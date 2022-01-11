b64d45ba1be52e4315275e2906ac6a17d60d2714
Kostya Lazanchuk <90553568+KostyaLazanchuk@users.noreply.github.com>
2022-01-11 17:24:15 +0200

    Add project files.

---
+++ Lab1/Caesar.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    static internal class Caesar
+    {
+        /*public static char cipher(char ch, int key)
+        {
+            if (!char.IsLetter(ch))
+            {
+
+                return ch;
+            }
+
+            char d = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch + key) - d) % 26) + d);
+
+
+        }
+
+        public static string Encryption (string input, int key)
+        {
+            string output = string.Empty;
+
+            foreach(char ch in input)
+            {
+                output += cipher(ch, key);
+            }
+
+        }*/
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        public static char Cipher(char ch, int key)
+        {
+            if (!char.IsLetter(ch))
+            {
+                return ch;
+            }
+
+            char d = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch + key) - d) % 26) + d);
+        }
+
+
+        public static string Encipher(string input, int key)
+        {
+            string output = string.Empty;
+
+            foreach (char ch in input)
+                output += Cipher(ch, key);
+
+            return output;
+        }
+
+        public static string Decipher(string input, int key)
+        {
+            return Encipher(input, 26 - key);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a string to encrypt:");
+            string UserString = Console.ReadLine();
+
+            Console.WriteLine("\n");
+
+            Console.Write("Enter your Key");
+            Console.WriteLine("\n");
+
+
+            Console.WriteLine("Encrypted Data");
+            for (int key = 0; key < 26; key++)
+            {
+                string cipherText = Encipher(UserString, key);
+                Console.WriteLine(key + "number " + cipherText);
+                Console.Write("\n");
+            }
+
+            /*
+                        Console.WriteLine("Decrypted Data:");
+
+                        string t = Decipher(cipherText, key);
+                        Console.WriteLine(t);
+                        Console.Write("\n");*/
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
